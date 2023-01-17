f3714ccebec5c9188cc11cc13b6c98bbf6af239c
Robin Quint <rob2309@hotmail.de>
2023-01-17 15:27:30 +0100

    Added better tests

--- tests/ensure_derive_works.rs
+++ tests/ensure_derive_works.rs
@@ -1,51 +1,109 @@
+#![allow(unused)]
+
 use derive_debug::Dbg;
 
 #[derive(Dbg, Default)]
-#[dbg(alias = "TestAAlias")]
-struct TestA {
-    normal_field: (),
+#[dbg(alias = "TestStructAlias")]
+struct TestStruct {
+    plain_field: u32,
     #[dbg(skip)]
-    skipped_field: (),
+    skipped_field: u32,
+    #[dbg(alias = "alias_field_alias")]
+    alias_field: u32,
     #[dbg(placeholder = "...")]
-    placeholder_field: (),
+    placeholder_field: u32,
+    #[dbg(alias = "alias_placeholder_field_alias", placeholder = "abc")]
+    alias_placeholder_field: u32,
 }
 
 #[test]
-fn test_a_output() {
-    let a = TestA::default();
+fn test_struct() {
+    let foo = TestStruct::default();
 
     assert_eq!(
-        format!("{:?}", a),
-        "TestAAlias { normal_field: (), placeholder_field: ... }"
+        format!("\n{:#?}\n", foo),
+        r#"
+TestStructAlias {
+    plain_field: 0,
+    alias_field_alias: 0,
+    placeholder_field: ...,
+    alias_placeholder_field_alias: abc,
+}
+"#
     );
 }
 
 #[derive(Dbg)]
-enum TestB {
+enum TestEnum {
     UnitVariant,
-    TupleVariant(u32, bool),
-    StructVariant {
-        a: bool,
-        b: u32,
-    },
+    TupleVariant(u32, u32),
+    StructVariant{a: u32, b: u32},
 
     #[dbg(skip)]
     SkippedUnitVariant,
     #[dbg(skip)]
-    SkippedTupleVarian(u32, bool),
+    SkippedTupleVariant(u32, u32),
     #[dbg(skip)]
-    SkippedStructVariant {
-        a: bool,
-        b: u32,
-    },
+    SkippedStructVariant{a: u32, b: u32},
 
-    SkipTupleField(u32, #[dbg(skip)] bool, u32),
-    SkipStructField {
-        a: bool,
-        #[dbg(skip)]
-        b: bool,
-    },
+    #[dbg(alias = "AliasVariant")]
+    AliasedUnitVariant,
+    #[dbg(alias = "AliasVariant")]
+    AliasedTupleVariant(u32, u32),
+    #[dbg(alias = "AliasVariant")]
+    AliasedStructVariant{a: u32, b: u32},
+}
 
-    #[dbg(alias = "NotAliasVariant")]
-    AliasVariant(u32),
+#[test]
+fn test_unit_variant() {
+    let foo = TestEnum::UnitVariant;
+    assert_eq!(format!("{:?}", foo), "UnitVariant");
+}
+
+#[test]
+fn test_tuple_variant() {
+    let foo = TestEnum::TupleVariant(0, 1);
+    assert_eq!(format!("{:?}", foo), "TupleVariant(0, 1)");
+}
+
+#[test]
+fn test_struct_variant() {
+    let foo = TestEnum::StructVariant { a: 0, b: 1 };
+    assert_eq!(format!("{:?}", foo), "StructVariant { a: 0, b: 1 }");
+}
+
+#[test]
+fn test_skipped_unit_variant() {
+    let foo = TestEnum::SkippedUnitVariant;
+    assert_eq!(format!("{:?}", foo), "SkippedUnitVariant");
+}
+
+#[test]
+fn test_skipped_tuple_variant() {
+    let foo = TestEnum::SkippedTupleVariant(0, 1);
+    assert_eq!(format!("{:?}", foo), "SkippedTupleVariant");
+}
+
+#[test]
+fn test_skipped_struct_variant() {
+    let foo = TestEnum::SkippedStructVariant { a: 0, b: 1 };
+    assert_eq!(format!("{:?}", foo), "SkippedStructVariant");
+}
+
+#[test]
+fn test_aliased_unit_variant() {
+    let foo = TestEnum::AliasedUnitVariant;
+    assert_eq!(format!("{:?}", foo), "AliasVariant");
+}
+
+#[test]
+fn test_aliased_tuple_variant() {
+    let foo = TestEnum::AliasedTupleVariant(0, 1);
+    assert_eq!(format!("{:?}", foo), "AliasVariant(0, 1)");
+}
+
+#[test]
+fn test_aliased_struct_variant() {
+    let foo = TestEnum::AliasedStructVariant { a: 0, b: 1 };
+    assert_eq!(format!("{:?}", foo), "AliasVariant { a: 0, b: 1 }");
 }
